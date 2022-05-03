b61b571664b1d08be76ddc8f56d1acb49aa37e2c
Raquel Dantas <96212455+raqueueuel@users.noreply.github.com>
2022-05-02 21:50:41 -0300

    alterações

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const express = require ("express");
+const exp = require ("express");
+const app = exp ();
+const porta = process.env.PORT || 3000
+app.get('/', (req,res)=>{
+    app.send('heroku')
+})
 
-
+app.listen(porta, ()=> {
+    console.log("estou rodando no heroku!")
+});
+a
